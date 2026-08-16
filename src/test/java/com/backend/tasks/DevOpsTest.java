ae0df0d9af9dd1bbc50e922ed693071737947f7b
Cris <nombredeusuario9974@gmail.com>
2026-08-16 19:11:29 +0200

    Añadir workflow de GitHub Actions

---
+++ src/test/java/com/backend/tasks/DevOpsTest.java
@@ -0,0 +1,17 @@
+package com.backend.tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DevOpsTest {
+	
+	@Test
+	void sumaDebeSerCorrecta() {
+		int resultado = 2 +2;
+		
+		assertEquals(4, resultado);
+	}
+	
+	
+	
+}
